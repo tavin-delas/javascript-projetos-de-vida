9adf5de034289d23ccb8b0f50428f300c4739f2a
tavin-delas <luiz.machado.cruz@escola.pr.gov.br>
2025-07-29 08:31:38 -0300

    .

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ for(let i = 0; i< botoes.length; i++){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+
+
+    const contadores = document.querySelectorAll(".contador");
+    const tempoObjetivo1 = nem Date("2025-10-05T00:00:00");
+
+    let tempoAtual = nem Date ();
+    contadores[0].textContent = tempoObjetivo1 - tempoAtual 
+
+    function calculaTempo(tempoObjetivo){
+        let tempoAtual = new Date();
+        let tempoFinal = tempoObjetivo - tempoAtual;
+        let segundos = Math.floor(tempoFinal/1000);
+        let minutos = Math.floor(segundos/60);
+        let horas = Math.floor(minutos/60);
+        let horas = Math.floor(horas/24);
+
+        segundos %=60;
+        minutos %=60;
+        horas %=24;
+
+        return dias + " dias " + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    
+    }
